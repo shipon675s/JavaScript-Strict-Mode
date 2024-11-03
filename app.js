cd110480f26d1e3d99ec2f1fcf146c49e803a7dc
Md. Ibrahim Shipon <156697977+shipon675s@users.noreply.github.com>
2024-11-03 22:55:41 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//! JavaScript Strict Mode
+
+'use strict'
+let x = 5;
+console.log(x);
+
+name(5, 4);
+function name(p1, p2) {
+    // 'use strict'
+    y = 5;
+    console.log(p1, p2);
+}
+
+
+/* 
+
+*   Future reserved word not uses ⬇ 
+let private = 'Some text';
+let public = 'Some text';
+let package = 'Some text';
+
+*/
+
+
+/* 
+
+*   Future reserved word not uses ⬇ 
+let while = 'Some text';
+
+*/
